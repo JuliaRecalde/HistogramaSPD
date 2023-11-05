d215ade5311bc8f043573a5b857f96d7568b34d9
JuliaRecalde <yuliantonia@gmail.com>
2023-11-05 20:23:51 -0300

    commit

--- paralelo.c
+++ paralelo.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
+#include <omp.h>
 
 #define NUM_CATEGORIAS 6
 #define NUM_ESTADOS 54
+omp_lock_t hist_locks[NUM_CATEGORIAS]; 
 
 char *nombre_categorias[NUM_CATEGORIAS] = {
     "Children 0-18",
@@ -22,10 +23,20 @@ void procesarEstado(const char *line, int indiceEstado) {
     int categorias[NUM_CATEGORIAS] = {0};
     sscanf(line, "%*[^,],%d,%d,%d,%d,%d,%d\n",
            &categorias[0], &categorias[1], &categorias[2], &categorias[3],
-           &categorias[4], &categorias[5]);
+           &categorias[4], &categorias[5]);   
     if (strcmp(estados[indiceEstado], "United States") != 0) {
+        #pragma omp parallel for
         for (int i = 0; i < NUM_CATEGORIAS; i++) {
+            omp_init_lock(&hist_locks[i]);
+        }
+        #pragma omp parallel for
+        for (int i = 0; i < NUM_CATEGORIAS; i++) {
+            omp_set_lock(&hist_locks[i]);
             histograma_final[i] += categorias[i];
+            omp_unset_lock(&hist_locks[i]);
+        }   
+        for (int i = 0; i < NUM_CATEGORIAS; i++) {
+            omp_destroy_lock(&hist_locks[i]);
         }
     }
 }
@@ -54,6 +65,7 @@ void procesarArchivo()
     fclose(file);
 }
 
+
 void mostrarHistograma() {
     printf("Histograma final:\n");
     int escala = 10000000;
@@ -67,11 +79,13 @@ void mostrarHistograma() {
 }
 
 int main() {
-    double inicio = time(NULL);
+    double inicio, fin, tiempo;
+    inicio = omp_get_wtime();
     abrirArchivo();
     procesarArchivo();
     mostrarHistograma();
-    double fin = time(NULL);
-    printf("Tiempo de ejecución: %f segundos\n", fin - inicio);
+    fin = omp_get_wtime();
+    tiempo = fin - inicio;
+    printf("Tiempo de ejecución: %f segundos\n", tiempo);
     return 0;
 }
